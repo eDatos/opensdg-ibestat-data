520fb4b40886bddda015448842cac4d8b2bc1e06
F. Javier Rodríguez García <frodgar@arte-consultores.com>
2025-07-15 15:14:05 +0100

    EDATOS-4872 - Modificar la generación de la página de ODS para generarla desde la API de cubos en vez de desde CSV
    > Modificado modo en que se limpia el HTML para conservar los br

--- scripts/edatos/utils/html.py
+++ scripts/edatos/utils/html.py
@@ -2,4 +2,8 @@ from bs4 import BeautifulSoup
 
 def remove_tags(html):
     soup = BeautifulSoup(html, 'html.parser')
-    return soup.get_text() 
+    whitelist=['br']
+    for tag in soup.find_all(True):
+        if tag.name not in whitelist:
+            tag.unwrap()
+    return soup.decode_contents() 
