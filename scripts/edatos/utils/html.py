e7d8c16b70593b26a36bc4f2755f439044eff5c3
F. Javier Rodríguez García <frodgar@arte-consultores.com>
2025-07-16 11:20:50 +0100

    EDATOS-4872 - Modificar la generación de la página de ODS para generarla desde la API de cubos en vez de desde CSV
    > Modificado modo en que se limpia el HTML para que no codifique los <> y demás simbolos

--- scripts/edatos/utils/html.py
+++ scripts/edatos/utils/html.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
+import html
 
-def remove_tags(html):
-    soup = BeautifulSoup(html, 'html.parser')
+def remove_tags(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
     whitelist=['br']
     for tag in soup.find_all(True):
         if tag.name not in whitelist:
             tag.unwrap()
-    return soup.decode_contents() 
+    return html.unescape(soup.decode_contents())
