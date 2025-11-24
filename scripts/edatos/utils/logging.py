ff96fd16a3d84ba57216bcc303e57c61bea882e2
F. Javier Rodríguez García <frodgar@arte-consultores.com>
2025-11-24 17:01:28 +0000

    EDATOS-4872 - Modificar la generación de la página de ODS para generarla desde la API de cubos en vez de desde CSV
    > Añadida configuración de logger

---
+++ scripts/edatos/utils/logging.py
@@ -0,0 +1,39 @@
+
+import logging
+from io import StringIO as StringBuffer
+import sys
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+### Setup the console handler with a StringIO object
+### One for all the modules to use
+log_capture_string = StringBuffer()
+# log_capture_string.encoding = 'cp1251'
+variableHandler = logging.StreamHandler(log_capture_string)
+variableHandler.setLevel(logging.WARNING)
+variableHandler.setFormatter(formatter)
+
+consoleHandler = logging.StreamHandler(sys.stdout)
+consoleHandler.setLevel(logging.DEBUG)
+consoleHandler.setFormatter(formatter)
+
+fileHandler = logging.FileHandler("execution.log", 'w+')
+fileHandler.setLevel(logging.INFO)
+fileHandler.setFormatter(formatter)
+
+logger = logging.getLogger()   
+logger.setLevel(logging.INFO) 
+logger.addHandler(variableHandler)
+logger.addHandler(consoleHandler)
+# ONLY local development
+# logger.addHandler(fileHandler)
+
+def getLogger(clazz):    
+    logger = logging.getLogger(clazz)        
+    return logger
+
+def getLogMessages():
+    ### Pull the contents back into a string and close the stream
+    log_contents = log_capture_string.getvalue()
+    log_capture_string.close()
+    return log_contents
