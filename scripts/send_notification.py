2e41a8c7957d566360bc7e741e37312bd614030d
F. Javier Rodríguez García <frodgar@arte-consultores.com>
2025-11-28 14:33:15 +0000

    EDATOS-4872 - Modificar la generación de la página de ODS para generarla desde la API de cubos en vez de desde CSV
    > Modificamos send_notification para que envie desde fichero

--- scripts/send_notification.py
+++ scripts/send_notification.py
@@ -1,13 +1,31 @@
+import sys
 from edatos.services.notices import NoticesClient, buildNotice
+from edatos.utils.logging import getLogger
+
+logger = getLogger("send_notification")
 
 if __name__ == '__main__':
 
+    if len(sys.argv) < 2:
+            print("Uso: poetry run python send_notification.py <archivo_texto>")
+            sys.exit(1)
+
+    archivo = sys.argv[1]
+
+    try:
+        with open(archivo, "r", encoding="utf-8") as f:
+            contenido = f.read().strip()
+    except Exception as e:
+        logger.error(f"Error al leer el archivo '{archivo}': {e}")
+        sys.exit(1)
+
+
     client = NoticesClient('https://estadisticas.arte-consultores.com/notices-internal/apis/notices-internal/v1.0')   
-    notice = buildNotice("holi")
+    notice = buildNotice(contenido)
     
     try:
         response = client.create_notice(notice)
-        print(f"Notificación creada exitosamente. Status: {response.status_code}")
-        print(f"Respuesta: {response.text}")
+        logger.info(f"Notificación creada exitosamente. Status: {response.status_code}")
+        logger.info(f"Respuesta: {response.text}")
     except Exception as e:
-        print(f"Error al crear notificación: {e}")
+        logger.error(f"Error al crear notificación: {e}")
