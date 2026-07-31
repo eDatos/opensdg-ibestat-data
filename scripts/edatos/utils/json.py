5ba1128168cb78fbd6d04a31dec22225279f1ec4
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2026-07-31 08:24:55 +0100

    EDATOS-5874 - feat: incluye api-key en las llamadas que se hagan a la API

--- scripts/edatos/utils/json.py
+++ scripts/edatos/utils/json.py
@@ -1,6 +1,10 @@
+import os
+
 import requests
 
+
 def download(url):
-    response = requests.get(url)
+    api_key = os.environ.get("ODS_LOAD_API_KEY")
+    response = requests.get(url, headers={"api-key": api_key} if api_key else None)
     response.raise_for_status()
     return response.json()
